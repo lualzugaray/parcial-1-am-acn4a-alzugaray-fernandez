a4e2ab684dc289c06d109643d845ef21410480b2
lualzugaray <lucia.alzugaray24@gmail.com>
2023-10-21 02:32:03 -0300

    Carga del proyecto

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private LinearLayout linearLayout;
+    private ScrollView scrollView;
+    private int elementCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        scrollView = findViewById(R.id.scrollView);
+        linearLayout = findViewById(R.id.linearLayout); // Agregar esta línea para encontrar el LinearLayout
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Botón de inicio de sesión presionado", Toast.LENGTH_SHORT).show();
+                // Agrega lógica de inicio de sesión aquí
+            }
+        });
+    }
+
+    public void forgotPasswordClick(View view) {
+        // Lógica cuando se hace clic en "Olvidé mi contraseña"
+    }
+
+    public void createAccountClick(View view) {
+        // Lógica cuando se hace clic en "Crear una cuenta nueva"
+
+        // Agregar un nuevo elemento de texto al ScrollView dinámicamente
+        TextView textView = new TextView(this);
+        textView.setText("Cuenta " + (elementCount + 1) + " creada");
+        linearLayout.addView(textView);
+        elementCount++;
+    }
+}
