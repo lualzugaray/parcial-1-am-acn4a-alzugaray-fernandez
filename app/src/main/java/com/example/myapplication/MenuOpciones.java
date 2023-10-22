b33d0343f7f242891b5451ac3ac88749a73c582b
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-10-22 13:35:24 +0200

    add: evento simple, mensaje pop up al hacer click en el carrito de compras

--- app/src/main/java/com/example/myapplication/MenuOpciones.java
+++ app/src/main/java/com/example/myapplication/MenuOpciones.java
@@ -11,14 +11,21 @@ import android.widget.Toast;
 
 public class MenuOpciones extends AppCompatActivity {
 
-
+    private ImageView carro_compras;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_opciones);
 
 
+        carro_compras = findViewById(R.id.img_cart);
+        carro_compras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Carro de compras vacio", Toast.LENGTH_SHORT).show();
 
+            }
+        });
 
 
     }
