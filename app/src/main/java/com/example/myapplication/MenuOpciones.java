6d716d2fc33b60f0a950245efcd7d8338394889a
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-10-22 16:15:52 +0200

    style: reacomodacion de la barra de busqueda en activity Menu Opciones. Se agregan imagenes para usar en botones de categorias en la misma activity. add: evento para la barra de busqueda, aun no funciona.

--- app/src/main/java/com/example/myapplication/MenuOpciones.java
+++ app/src/main/java/com/example/myapplication/MenuOpciones.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TableLayout;
@@ -15,15 +16,16 @@ import android.widget.Toast;
 
 public class MenuOpciones extends AppCompatActivity {
 
-    private ImageView carro_compras;
+    private ImageView carroCompras;
+    private EditText textoBuscador;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_opciones);
 
 
-        carro_compras = findViewById(R.id.img_cart);
-        carro_compras.setOnClickListener(new View.OnClickListener() {
+        carroCompras = findViewById(R.id.img_cart);
+        carroCompras.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Carro de compras vacio", Toast.LENGTH_SHORT).show();
@@ -31,6 +33,18 @@ public class MenuOpciones extends AppCompatActivity {
             }
         });
 
+        textoBuscador = findViewById(R.id.etBuscador);
+
+        textoBuscador.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    textoBuscador.setHint("");
+                } else {
+                    textoBuscador.setHint("¿Qué estás buscando?");
+                }
+            }
+        });
     }
 
     @Override
