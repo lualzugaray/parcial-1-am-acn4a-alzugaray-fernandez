375df3c5deda8727f8caf866d0e4c7a7b85586f9
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-10-22 12:55:50 +0200

    Avances activity de menu de opciones. Ediciones en estilo de botones. Se agrega evento onClick a imagen de carro de compras.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -52,5 +53,7 @@ public class MainActivity extends Activity {
         elementCount++;
     }
 
-    
+
+
+
 }
