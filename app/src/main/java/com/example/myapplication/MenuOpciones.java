8daec039d63ba4b76891d5121d23ea2a03383b95
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-10-22 13:16:31 +0200

    delete: eliminacion de activity "MenuPrincipal" y reemplazo de la misma por "MenuOpciones"

---
+++ app/src/main/java/com/example/myapplication/MenuOpciones.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TableLayout;
+import android.widget.Toast;
+
+public class MenuOpciones extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_opciones);
+
+
+
+
+
+    }
+}
