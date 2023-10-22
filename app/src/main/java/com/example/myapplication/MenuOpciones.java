0775e6b8fc4d3a958017285c42c48a264c599656
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-10-22 14:38:20 +0200

    add: evento onKeyDown en activity MenuOpciones

--- app/src/main/java/com/example/myapplication/MenuOpciones.java
+++ app/src/main/java/com/example/myapplication/MenuOpciones.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
+import android.content.DialogInterface;
+import android.content.Intent;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SearchView;
@@ -20,13 +24,35 @@ public class MenuOpciones extends AppCompatActivity {
 
         carro_compras = findViewById(R.id.img_cart);
         carro_compras.setOnClickListener(new View.OnClickListener() {
-            @Override
+
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Carro de compras vacio", Toast.LENGTH_SHORT).show();
 
             }
         });
 
+    }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==event.KEYCODE_BACK){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("¿Desea salir?").setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+                    builder.show();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
