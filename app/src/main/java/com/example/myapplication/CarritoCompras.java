67317960a7b81e7a7fbab3107468ccefc099086e
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-11-24 13:54:06 +0100

    fix: correccion para agregar activities en el manifesto

---
+++ app/src/main/java/com/example/myapplication/CarritoCompras.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplication.databinding.ActivityCarritoComprasBinding;
+
+public class CarritoCompras extends AppCompatActivity {
+
+    private ActivityCarritoComprasBinding binding;
+
+
+}
